1614c899f087d87d1fbbd622ae27c156a7f2d83c
ahmedyousry0098 <ahmedyousry098@gmail.com>
2023-08-28 21:57:28 +0300

    fix admin guard

--- src/guards/isAdmin.guard.ts
+++ src/guards/isAdmin.guard.ts
@@ -1,11 +1,12 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { GENERALROLE } from 'src/users/enums/user.role'
 
 @Injectable()
 export class AdminGuard implements CanActivate {
     constructor() {}
 
-    canActivate(context: ExecutionContext): Promise<boolean> {
+    canActivate(context: ExecutionContext): Promise<boolean> | boolean {
         const request = context.switchToHttp().getRequest()
-        return request.user?.isAdmin
+        return request.user?.role.toLowerCase() == GENERALROLE.ADMIN
     }
 }
